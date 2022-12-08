79117ae4e3d05f36ee5f722fc5b2e4edddcfbe15
caveman <948238104@qq.com>
2022-12-08 18:04:47 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static final int WIDTH = 1000;
+	public static final int HEIGHT = 1000;
+
+	public static void main(String[] args) {
+		JFrame win = new JFrame();
+		win.setVisible(true);
+		win.setSize(WIDTH, HEIGHT);
+		win.setDefaultCloseOperation(3);
+		win.setLocationRelativeTo(null);
+
+		ChessBoardPanel panle = new ChessBoardPanel();
+		win.add(panle);
+
+		panle.run();
+	}
+
+}
